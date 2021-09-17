18b82884623e0610f47cb2a3da23c385e1a8a1a8
MauricioDonato <mau.donato@duocuc.cl>
2021-09-16 23:28:19 -0300

    comienzo restablecer contraseña

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -15,6 +15,10 @@ const routes: Routes = [
     path: 'ingresar',
     loadChildren: () => import('./ingresar/ingresar.module').then( m => m.IngresarPageModule)
   },
+  {
+    path: 'restablecer-contrasena',
+    loadChildren: () => import('./restablecer-contrasena/restablecer-contrasena.module').then( m => m.RestablecerContrasenaPageModule)
+  },
 ];
 
 @NgModule({
--- src/app/ingresar/ingresar.page.ts
+++ src/app/ingresar/ingresar.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-ingresar',
@@ -6,10 +8,52 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./ingresar.page.scss'],
 })
 export class IngresarPage implements OnInit {
+  contrasena_v : string;
+  nombre_v : string;
 
-  constructor() { }
-
+  constructor(private alertControl: AlertController) { }
   ngOnInit() {
   }
+  async validar_ingreso (nombre: HTMLInputElement, contrasena: HTMLInputElement)
+  {
+    let name = nombre.value;
+    let password = contrasena.value;
+    this.nombre_v = "joshua1234";
+    this.contrasena_v = "1234";
+     if (name.trim() == "")
+     {
+       const alerta = await this.alertControl.create({
+         header: "Falta información",
+         message: "Por favor ingrese información en el campo nombre",
+       });
+       await alerta.present();
+     }
+
+     else if (password.trim() == "")
+     {
+       const alerta = await this.alertControl.create({
+         header: "Falta información",
+         message: "Por favor ingrese información en el campo contraseña"
+       });
+       await alerta.present();
+     };
+     if (name.trim() != this.nombre_v)
+     {
+       const alerta = await this.alertControl.create({
+         header: "Nombre incorrecto",
+         message: "Por favor ingrese información correcta",
+       });
+       await alerta.present();
+     }
+     else if (password.trim() != this.contrasena_v)
+     {
+       const alerta = await this.alertControl.create({
+         header: "Contraseña incorrecta",
+         message: "Por favor ingrese información correcta"
+       });
+       await alerta.present();
+     };
+  }
+  
 
 }
